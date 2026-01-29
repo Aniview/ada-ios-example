01f5ce71a70f1825ce4580464124ee04cbfda818
Pavlo Yevtukhov <pavlo.yevtukhov@globallogic.com>
2026-01-29 16:56:00 +0200

    update example to use v1.0.0

--- AdaExample/AdaExample/Examples/BasicVC.swift
+++ AdaExample/AdaExample/Examples/BasicVC.swift
@@ -8,15 +8,22 @@
 import UIKit
 import AdaSdk
 
-class ViewController: UIViewController {
+final class BasicVC: UIViewController {
+    private let config: AdaConfig
+
+    init(config: AdaConfig) {
+        self.config = config
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Configure the ad
-        let config = AdaConfig(
-            pubId: "565c56d3181f46bd608b459a",
-            tagId: "68b00e4a7c1583367d03f437"
-        )
+       // let config: AdaConfig = .prod
 
         // Create the ad view
         let adView = AdaView(config: config)
@@ -38,13 +45,13 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: AdaViewDelegate {
+extension BasicVC: AdaViewDelegate {
     func onEvent(_ event: AdaSdk.AdaEvent) {
         print("Ada event: \(event)")
     }
 }
 
-extension ViewController: SizeDelegate {
+extension BasicVC: SizeDelegate {
     func onHeightChange(_ height: CGFloat) {
         print("Ada view needs height: \(height)")
     }
---
+++ AdaExample/AdaExample/Examples/PreloadExampleVC.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  AdaExample
+//
+//  Created by Pavlo Yevtukhov on 27.08.2025.
+//
+
+import AdaSdk
+import UIKit
+
+final class PreloadExampleVC: UIViewController {
+    private let config: AdaConfig
+
+    init(config: AdaConfig) {
+        self.config = config
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var adView: AdaView = {
+        let view = AdaView(config: config)
+        view.delegate = self
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        adView.startLoading()
+    }
+}
+
+extension PreloadExampleVC: AdaViewDelegate {
+    func onEvent(_ event: AdaSdk.AdaEvent) {
+
+        print("Ada event: \(event)")
+
+        switch event {
+        case .onImpression:
+            guard adView.superview == nil else { return }
+            view.addSubview(adView)
+            adView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                adView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                adView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                adView.widthAnchor.constraint(equalTo: view.widthAnchor)
+            ])
+        default:
+            break
+        }
+    }
+}
+
